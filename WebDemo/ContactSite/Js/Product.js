78684fdc663ba809b46bcdf7eb013e87a9f53202
Ray <cguangzhi@163.com>
2017-05-01 22:50:06 +0800

    add some content

--- WebDemo/ContactSite/Js/Product.js
+++ WebDemo/ContactSite/Js/Product.js
@@ -1,1 +1,47 @@
-﻿
+﻿var Product = Backbone.Model.extend({
+    defaults : function(){
+        productID : -1;
+        Name: "";
+        Category: "unclassed"
+        Price : 0
+    },
+    AddProduct: function () {
+        // doing Add Product
+    },
+    EditProduct: function () {
+        // will load the edit product form
+    },
+    DeleteProduct: function () {
+        // wil doing delte product
+    },
+    Initialize : function()
+    {
+        // include intialize, and the events listen
+    }
+});
+
+var Products = Backbone.Model.extend({
+    model: Product
+});
+   
+var ProductView = Backbone.View.extend({
+    model: Product,
+    render: function () {
+        // to load the add/delete product view function
+    },
+    save: function () {
+        // If Product ID is 0, then create new Product
+        // else, doing delete 
+    },
+    Delete: function () {
+        // call web service to delete an exieted product
+    }
+})
+
+var ProductMrgView = Backbone.View.extend({
+    model: Products,
+    Initialize: function ()
+    {
+        // load the data from webservice
+    }
+})
